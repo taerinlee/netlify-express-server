5f84e11d8eb5a6b0861602b6aafb54587a2ca51e
Taerin Lee <taerin@favoritemedium.net>
2021-03-08 21:56:06 +0900

    change file name

--- express/server.js
+++ express/server.js
@@ -5,12 +5,12 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const baseroute = require('./base.route');
+const baseRoute = require('./baseRoute');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', baseroute);  // path must route to lambda
+app.use('/.netlify/functions/server', baseRoute);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
