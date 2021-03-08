db7e169eb3a025669a3d5b2ab2da060c8d2bd1ef
Taerin Lee <taerin@favoritemedium.net>
2021-03-08 21:30:24 +0900

    delete post api

--- express/DB_develop.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DB: 'mongodb+srv://admin:pwHera!1@cluster0.jad0a.mongodb.net/mevncrud?retryWrites=true&w=majority'
-}
--- express/server.js
+++ express/server.js
@@ -5,21 +5,11 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./DB_develop.js');
 const baseRoute = require('./base.route');
-const postRoute = require('./post.route');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => { console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/posts', postRoute);
 app.use('/.netlify/functions/server', baseRoute);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
