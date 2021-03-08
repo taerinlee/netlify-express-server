7ad8c0e9c528e852600258c66c0a9f11c50319c9
Taerin Lee <taerin@favoritemedium.net>
2021-03-08 23:33:04 +0900

    connect to mongo db

--- express/baseRoute.js
+++ express/baseRoute.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// Require Post model in our routes module
+let postModel = require('./postModel');
+
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  postModel.find((err, posts) => {
+    if(err){
+      res.json(err);
+    }
+    else {
+      res.json(posts);
+    }
+  });
 });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
