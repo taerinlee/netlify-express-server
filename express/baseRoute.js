b05a2aa31c7da877d9470378fc929d50b2cb1125
Taerin Lee <taerin@favoritemedium.net>
2021-03-09 14:33:03 +0900

    move functions for post to basic route

--- express/baseRoute.js
+++ express/baseRoute.js
@@ -15,8 +15,53 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// Defined store route
+router.post('/add', (req, res) => {
+  let body = new postModel(req.body);
+  body.save()
+    .then(() => {
+      res.status(200).json({'business': 'business in added successfully'});
+    })
+    .catch(() => {
+      res.status(400).send("unable to save to database");
+    });
+});
 
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// Defined get a post route
+router.get('/post/:id', (req, res) => {
+  let id = req.params.id;
+  postModel.findById(id, (err, post) => {
+      if(err) {
+        res.json(err);
+      }
+      res.json(post);
+  });
+});
+
+//  Defined update route
+router.post('/update/:id', (req, res) => {
+  postModel.findById(req.params.id, (err, post) => {
+    if (!post)
+      res.status(404).send("data is not found");
+    else {
+        post.title = req.body.title;
+        post.body = req.body.body;
+        post.save().then(() => {
+          res.json('Update complete');
+      })
+      .catch(() => {
+            res.status(400).send("unable to update the database");
+      });
+    }
+  });
+});
+
+// Defined delete | remove | destroy route
+router.delete('/delete/:id', (req, res) => {
+  postModel.findByIdAndRemove({_id: req.params.id}, (err) => {
+    if(err) res.json(err);
+    else res.json('Successfully removed');
+  });
+});
 
 module.exports = router;
